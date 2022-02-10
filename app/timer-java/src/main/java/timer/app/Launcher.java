78a557e5c79c26f82f059a30c2a5787cc303974c
KaranVani <karan.s.vani@gmail.com>
2022-02-10 22:45:34 +0000

    Added switchability between 2 scences using buttons

--- app/timer-java/src/main/java/timer/app/Launcher.java
+++ app/timer-java/src/main/java/timer/app/Launcher.java
@@ -3,10 +3,15 @@ package timer.app;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
+
+  Stage window;
+  Scene intro, picture;
 
   public static void main(String[] args) {
     launch(args);
@@ -14,17 +19,31 @@ public class Launcher extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-    stage.setTitle("Timer");
-    Button button = new Button();
-    button.setText("Fetch API tingz");
 
-    // sets action for button when clicked using lambda expressions
-    button.setOnAction(e -> System.out.println("Hola"));
+    window = stage;
 
-    Scene scene = new Scene(new StackPane(button), 1280 , 720);
-    scene.getStylesheets().add("res/stylesheet.css");
-    stage.setScene(scene);
-    stage.show();
+    Label welcomemsg = new Label("This is the login screen");
+    Button logger = new Button("Login");
+    logger.setOnAction(e -> window.setScene(picture));
+
+    //Intro Login Layout
+    VBox intro1 = new VBox(20);
+    intro1.getChildren().addAll(welcomemsg,logger);
+    intro = new Scene(intro1, 1280, 720);
+
+    // Picture scene
+
+    Button previous = new Button("Logout");
+    previous.setOnAction(e -> window.setScene(intro));
+
+    // Picture Layout
+    StackPane picturelayout = new StackPane();
+    picturelayout.getChildren().addAll(previous);
+    picture = new Scene(picturelayout, 1280, 720);
+
+    window.setScene(intro);
+    window.setTitle("Timer");
+    window.show();
 
   }
 
