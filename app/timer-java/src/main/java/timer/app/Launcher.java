b321f8317bf2ed2de3776281328cf1dadc17e03f
KaranVani <karan.s.vani@gmail.com>
2022-02-10 20:39:19 +0000

    created a button that does absolutely nothing

--- app/timer-java/src/main/java/timer/app/Launcher.java
+++ app/timer-java/src/main/java/timer/app/Launcher.java
@@ -2,7 +2,7 @@ package timer.app;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -17,12 +17,13 @@ public class Launcher extends Application {
     stage.setTitle("Timer");
     String javaVersion = System.getProperty("java.version");
     String javafxVersion = System.getProperty("javafx.version1");
-    Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-    Scene scene = new Scene(new StackPane(l), 640, 480);
+    Button button = new Button();
+    button.setText("Fetch API tingz");
+    Scene scene = new Scene(new StackPane(button), 640, 480);
     stage.setScene(scene);
     stage.show();
-    
-    
+
+
   }
 
 }
