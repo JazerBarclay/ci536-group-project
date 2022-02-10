3d29718a0cbbf79914378f3c98ed05e044de9010
KaranVani <karan.s.vani@gmail.com>
2022-02-10 20:02:57 +0000

    added title

--- app/timer-java/src/main/java/timer/app/Launcher.java
+++ app/timer-java/src/main/java/timer/app/Launcher.java
@@ -14,13 +14,14 @@ public class Launcher extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-    
+    stage.setTitle("Timer");
     String javaVersion = System.getProperty("java.version");
     String javafxVersion = System.getProperty("javafx.version1");
     Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
     Scene scene = new Scene(new StackPane(l), 640, 480);
     stage.setScene(scene);
     stage.show();
+    
     
   }
 
