479426ce305dab7101a3a59115891a16a4fc9e08
Jazer <contact@jazer.co.uk>
2022-02-10 08:54:42 +0000

    Added script for running postgres via docker for testing

---
+++ database/pg_dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Kills any running instance of pgdb then launches a new container mounting the schema and seed sql files
+
+# Kill any running docker process for pgdb
+docker stop pgdb 2>/dev/null
+docker rm pgdb 2>/dev/null
+
+# Run new container under pgdb
+docker run -v $PWD/schema.sql:/docker-entrypoint-initdb.d/1-schema.sql -v $PWD/seed.sql:/docker-entrypoint-initdb.d/2.seed.sql -p 5432:5432 --name pgdb -e POSTGRES_USER=web -e POSTGRES_PASSWORD=pass -e POSTGRES_DB=blog -d postgres
