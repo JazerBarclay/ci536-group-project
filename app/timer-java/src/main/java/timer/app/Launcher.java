0505b8e69a9df2ec9616f65c3baa6939e437a3c7
Jazer <contact@jazer.co.uk>
2022-02-10 14:30:18 +0000

    Added boilerplate app for javafx

--- app/timer-java/src/main/java/timer/app/Launcher.java
+++ app/timer-java/src/main/java/timer/app/Launcher.java
@@ -1,9 +1,27 @@
 package timer.app;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
 
   public static void main(String[] args) {
-    System.out.println("Welcome to the timer app");
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    
+    String javaVersion = System.getProperty("java.version");
+    String javafxVersion = System.getProperty("javafx.version");
+    Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+    Scene scene = new Scene(new StackPane(l), 640, 480);
+    stage.setScene(scene);
+    stage.show();
+    
   }
 
 }
