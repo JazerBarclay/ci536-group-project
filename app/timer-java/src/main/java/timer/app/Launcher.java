7afb302c46864ad3d050d3783d98f90916e199ca
KaranVani <karan.s.vani@gmail.com>
2022-02-11 12:02:34 +0000

    added 2 scenes with buttons that swtch through both of them

--- app/timer-java/src/main/java/timer/app/Launcher.java
+++ app/timer-java/src/main/java/timer/app/Launcher.java
@@ -1,17 +1,18 @@
 package timer.app;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
 
   Stage window;
-  Scene intro, picture;
+  Scene scene1, scene2;
 
   public static void main(String[] args) {
     launch(args);
@@ -22,28 +23,64 @@ public class Launcher extends Application {
 
     window = stage;
 
-    Label welcomemsg = new Label("This is the login screen");
-    Button logger = new Button("Login");
-    logger.setOnAction(e -> window.setScene(picture));
+    GridPane grid = new GridPane();
+    grid.setPadding(new Insets(10,10,10,10));
+    grid.setVgap(8);
+    grid.setHgap(10);
 
-    //Intro Login Layout
-    VBox intro1 = new VBox(20);
-    intro1.getChildren().addAll(welcomemsg,logger);
-    intro = new Scene(intro1, 1280, 720);
+    GridPane grid2 = new GridPane();
+    grid2.setPadding(new Insets(10,10,10,10));
+    grid2.setVgap(8);
+    grid2.setHgap(10);
 
-    // Picture scene
+    // Intro Login Layout
+    // Username Label
+    Label userLabel = new Label("Username");
+    grid.add(userLabel,2,0);
 
-    Button previous = new Button("Logout");
-    previous.setOnAction(e -> window.setScene(intro));
+    // Username Input
+    TextField userInput = new TextField(("Username"));
+    grid.add(userInput,4,0);
 
-    // Picture Layout
-    StackPane picturelayout = new StackPane();
-    picturelayout.getChildren().addAll(previous);
-    picture = new Scene(picturelayout, 1280, 720);
+    // Password Label
+    Label passwordLabel = new Label("Password");
+    grid.add(passwordLabel,2,4);
 
-    window.setScene(intro);
-    window.setTitle("Timer");
+    // Password Input
+    TextField passinput = new TextField(("Test2"));
+    passinput.setPromptText("Password");
+    grid.add(passinput,4,4);
+
+    // Buttons
+    Button loginButton = new Button("Login");
+    loginButton.setOnAction(e -> {
+      window.setScene(scene2);
+    });
+    grid.add(loginButton,4,6);
+
+    Button previousButton= new Button("Previous button");
+    previousButton.setOnAction(e -> {
+      window.setScene(scene1);
+    });
+
+    grid2.add(previousButton,7,6);
+    scene2 = new Scene(grid2, 1280,720);
+
+
+    //Layout 1
+    Scene scene1 = new Scene(grid, 1280,720);
+    scene1.getStylesheets().add("res/stylesheet.css");
+
+    //Layout 2
+
+
+
+    window.setScene(scene1);
     window.show();
+    window.setTitle("Timer");
+
+
+
 
   }
 
