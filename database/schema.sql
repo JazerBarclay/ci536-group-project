84c74385516b1ff6cd0cd7cf87f08ddb8fa65dab
Jazer <contact@jazer.co.uk>
2022-02-11 18:22:48 +0000

    Added template tables for database

---
+++ database/schema.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS pomodoros;
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    user_email VARCHAR(60) NOT NULL UNIQUE,
+    user_username VARCHAR(40) NOT NULL UNIQUE,
+    user_password VARCHAR(40) NOT NULL,
+    user_active BOOLEAN NOT NULL DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS pomodoros (
+    pomodoro_id SERIAL PRIMARY KEY,
+    pomodoro_user_id INTEGER NOT NULL,
+    pomodoro_text VARCHAR(60) NOT NULL DEFAULT '',
+    pomodoro_start TIMESTAMP NOT NULL,
+    pomodoro_end TIMESTAMP NOT NULL
+);
