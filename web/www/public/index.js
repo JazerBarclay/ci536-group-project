8a21e6aa2af5d0cdc29ea55a0ad1e5243d3ed7ec
finlaywatling <finlaywatling@gmail.com>
2022-02-18 12:49:54 +0000

    added sign up page wip

--- web/www/public/index.js
+++ web/www/public/index.js
@@ -1,14 +1,16 @@
 window.addEventListener("load", () => {
 
 
+//to sign up button
+
 var signUpButton = document.querySelector('#signUpButton');
 
-
     signUpButton.addEventListener("click", function(evt){
-    
+
         evt.preventDefault();
+
         window.location.href = "signup.html";
-    
+        
     })
 
 
---
+++ web/www/public/signup.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+
+
+    //to login
+    
+    var logInButton = document.querySelector('#logInButton');
+    
+    logInButton.addEventListener("click", function(evt){
+    
+            evt.preventDefault();
+    
+            window.location.href = "index.html";
+            
+        })
+    
+    
+    
+    
+    
+    
+    
+    })
