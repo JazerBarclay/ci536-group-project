b40468223bc5f945428056693f8e5e2228e28200
finlaywatling <finlaywatling@gmail.com>
2022-02-21 13:28:51 +0000

    added user placeholder image and started implementing google log in functionality

--- web/www/public/index.js
+++ web/www/public/index.js
@@ -26,6 +26,17 @@ var logInButton = document.querySelector('#logInButton');
     })
 
 
+    //google sign in 
+
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      }
+
+
 
 
 
