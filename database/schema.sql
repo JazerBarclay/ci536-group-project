5982128f2e6b735b893614098db31e5bef726071
Vend3tta <ben.edmondson99@gmail.com>
2022-02-26 15:34:44 +0000

    added sessions and units table

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS pomodoros;
 DROP TABLE IF EXISTS groups;
-
+DROP TABLE IF EXISTS sessions_tbl;
+DROP TABLE IF EXISTS units;
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     user_email VARCHAR(60) NOT NULL UNIQUE,
@@ -21,4 +22,18 @@ CREATE TABLE IF NOT EXISTS pomodoros (
 CREATE TABLE IF NOT EXISTS groups (
   group_id INTEGER NOT NULL,
   group_name VARCHAR(60) NOT NULL UNIQUE,  
+);
+
+CREATE TABLE IF NOT EXISTS sessions_tbl ( 
+    session_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    session_note VARCHAR(200) NOT NULL DEFAULT '',
+    rating INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS units(
+    unit_id INTEGER NOT NULL,
+    session_id INTEGER NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL
 );
