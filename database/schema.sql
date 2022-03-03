d71a758939c21eb459491c055101b18b3f9833ad
Jazer <contact@jazer.co.uk>
2022-03-03 12:54:10 +0000

    Refactored schema tables

--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,8 @@
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS pomodoros;
 DROP TABLE IF EXISTS groups;
-DROP TABLE IF EXISTS sessions_tbl;
-DROP TABLE IF EXISTS units;
-DROP TABLE IF EXISTS usrGroups;
+DROP TABLE IF EXISTS user_groups;
+DROP TABLE IF EXISTS pomodoros;
+DROP TABLE IF EXISTS user_sessions;
 
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
@@ -11,38 +10,32 @@ CREATE TABLE IF NOT EXISTS users (
     user_username VARCHAR(40) NOT NULL UNIQUE,
     user_password VARCHAR(40) NOT NULL,
     user_active BOOLEAN NOT NULL DEFAULT false
-    avg_work_start TIMESTAMP NOT NULL,
-    avg_work_end TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS groups (
+    group_id SERIAL PRIMARY KEY,
+    group_name VARCHAR(60) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS user_groups (
+    user_id INTEGER NOT NULL REFERENCES users (user_id),
+    group_id INTEGER NOT NULL REFERENCES groups (group_id),
+    PRIMARY KEY (user_id, group_id)
 );
 
 CREATE TABLE IF NOT EXISTS pomodoros (
     pomodoro_id SERIAL PRIMARY KEY,
-    pomodoro_user_id INTEGER NOT NULL,
+    pomodoro_user_id INTEGER NOT NULL REFERENCES users (user_id),
     pomodoro_text VARCHAR(60) NOT NULL DEFAULT '',
     pomodoro_start TIMESTAMP NOT NULL,
     pomodoro_end TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS groups (
-    group_id INTEGER NOT NULL,
-    group_name VARCHAR(60) NOT NULL UNIQUE,  
-);
-
-CREATE TABLE IF NOT EXISTS sessions_tbl ( 
-    session_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS user_sessions ( 
+    user_session_id SERIAL PRIMARY KEY,
+    pomodoro_id INTEGER NOT NULL REFERENCES pomodoros (pomodoro_id),
     session_note VARCHAR(200) NOT NULL DEFAULT '',
+    session_start_time TIMESTAMP NOT NULL,
+    session_end_time TIMESTAMP NOT NULL,
     rating INTEGER NOT NULL DEFAULT 0
-);
-
-CREATE TABLE IF NOT EXISTS units (
-    unit_id INTEGER NOT NULL,
-    session_id INTEGER NOT NULL,
-    start_time TIMESTAMP NOT NULL,
-    end_time TIMESTAMP NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS usrGroups (
-    user_id INTEGER NOT NULL,
-    group_id INTEGER NOT NULL
 );
