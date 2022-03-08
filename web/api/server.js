a172021e98bd64d225976f2ee107efe642bed05d
Jazer <contact@jazer.co.uk>
2022-03-08 16:24:55 +0000

    Added route examples for db access

--- web/api/server.js
+++ web/api/server.js
@@ -29,14 +29,91 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('/user/:id', (req, res) => {
+// Register a new user on the system
+app.post('/register', (req,res) => {
 
+    // Received Parameters
+    // req.params.email, req.params.username, req.params.password
+
+    // Validate email, username and password meet minimum requirements
+
+    // Check username or email exists in db
+    // db.query('select', (error, result, fields) => { ... })
+
+    // If db returns > 0 then response.json failed due to existing user / email
+
+    // Add new user (salt + hash password)
+    // db.query('insert', (error, result, fields) => { ... })
+
+    // JSON response with insert successful
+
+})
+
+// Authenticate existing user to access the api and website
+app.post('/login', (req,res) => {
+
+    // Received Parameters
+    // req.params.email, req.params.password
+
+    // Check email is valid and passwords match (salt + hash to compare)
+    // db.query('select', (error, result, fields) => { ... })
+
+    // If details match, create token and send in response
+
+})
+
+// Return the profile matching the user token
+app.get('/profile', (req, res) => {
+
+    // Search db matching user id and token
+    // db.query('select', (error, result, fields) => { ... })
+
+    // Example response
+    res.status(200).json ({
+        username: "username", 
+        email: "email@address.xyz",
+        units: [
+            {
+                start: "10:00:00",
+                end: "10:25:00"
+            },
+            {
+                start: "10:30:00",
+                end: "10:55:00"
+            }
+        ]
+    })
+
+    // Example on-fail response
+    // res.status(406).json ({
+    //    status: 1, message: "Error, could not access the user profile"
+    // })
+
+
+})
+
+// Return the profile of a requested user by username
+app.get('/user/:username', (req, res) => {
+
+    // Query db
     db.query(
-        `SELECT * FROM users WHERE user_id = $1 LIMIT 1;`,
-        [req.params.id],
+        // Query term
+        `SELECT * FROM users WHERE user_username = $1;`,
+        // Parameters (referred to in query by $1,$2...)
+        [req.params.username],
+        // Callback function that returns either error, results with fields
         (error, results, fields) => {
-            if (error) res.status(500).json({error: "Internal Database Error"})
-            return res.status(200).json({results: results.rows[0]})
+            // If error exists, return 500 error code
+            if (error) return res.status(500).json({error: "Internal Database Error"})
+            // If no results found, return 404 error code
+            if (results.rows.length < 1)  return res.status(404).json({message: "User not found"})
+            // Since all checks pass, return requested object with 200 code
+            return res.status(200).json({
+                // Send username
+                username: results.rows[0].user_username,
+                // Send email
+                email: results.rows[0].user_email,
+            })
         }
     )
 
@@ -52,18 +129,6 @@ app.get('/scores', (req, res) => {
     // res.status(406).json ({
     //     status: 1, message: "Error! Server error!"
     // })
-})
-
-app.get('/profile', (req, res) => {
-    res.status(200).json ({
-        status: 1, message: "User profile accessed successfully"
-    })
-    // On Fail
-    // res.status(406).json ({
-    //    status: 1, message: "Error, could not access the user profile"
-    // })
-
-
 })
 
 
