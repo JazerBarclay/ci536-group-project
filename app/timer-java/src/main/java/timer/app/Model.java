cd810c9d0b38c1eea515a93748263582a642b5ae
KaranVani <karan.s.vani@gmail.com>
2022-03-11 17:38:02 +0000

    index on dev: 089f0c8 "styling fixes"

--- app/timer-java/src/main/java/timer/app/Model.java
+++ app/timer-java/src/main/java/timer/app/Model.java
@@ -7,79 +7,52 @@ import java.util.concurrent.TimeUnit;
 public class Model {
 
     private boolean loggedIn = false;
-    
+
     public Model() {
-	ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
-	exec.scheduleAtFixedRate(new Runnable() {
-	    @Override
-	    public void run() {
+        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+        exec.scheduleAtFixedRate(() -> {
 			updateTime();
 			if (listener != null) listener.onChange();
-	    }
-	}, 0, 1, TimeUnit.SECONDS);
+		}, 0, 1, TimeUnit.SECONDS);
     }
 
-    PropertyChangeListener listener = null;
 
-//    public long startTime;
-//	public long reverseTimer;
-//	long elapsedTime;
-//
-//	public void startTimer() {
-//		startTime = System.currentTimeMillis();
-//		reverseTimer = 1500;
-//		elapsedTime = System.currentTimeMillis() - startTime;
-//	}
-//
-//
-//	public void updateTime() {
-//		elapsedTime = System.currentTimeMillis() - startTime;
-//		elapsedTime /= 1000;
-//		reverseTimer -= elapsedTime;
-//		System.out.println(elapsedTime);
-//		System.out.println("New timer = " + reverseTimer);
-//    }
-//
-//	public long getSeconds() {
-//		return (reverseTimer % 60);
-//	}
-//
-//	public long getMinutes(){
-//		return (elapsedTime / 60);
-//	}
+	PropertyChangeListener listener = null;
 
 	public long starter, elapsedTimeSeconds, elapsedTimeMinutes;
-	public void startTimer() {
-		elapsedTimeMinutes = 24;
-		starter = 59;
-		elapsedTimeSeconds = 59;
-	}
+
+    public void startTimer() {
+        elapsedTimeMinutes = 24;
+        starter = 60;
+        elapsedTimeSeconds = 60;
+    }
 
 
-	public void updateTime() {
-		elapsedTimeSeconds--;
-		if(elapsedTimeSeconds == 0){
-			elapsedTimeSeconds = starter;
-			elapsedTimeMinutes--;
-		}
-	}
+    public void updateTime() {
+        elapsedTimeSeconds--;
+        if (elapsedTimeSeconds == 0) {
+            elapsedTimeSeconds = starter;
+            elapsedTimeMinutes--;
+        }
+		System.out.println("This is Listener = " + listener);
+    }
 
-	public long getSeconds() {
-		return (elapsedTimeSeconds);
-	}
+    public long getSeconds() {
+        return (elapsedTimeSeconds);
+    }
 
-	public long getMinutes(){
-		return (elapsedTimeMinutes);
-	}
+    public long getMinutes() {
+        return (elapsedTimeMinutes);
+    }
 
 //	---------------------------------------
 
     public void setChangeListener(PropertyChangeListener listener) {
-		this.listener = listener;
+        this.listener = listener;
     }
-    
+
     public void authenticated(boolean auth) {
-		this.loggedIn = auth;
-		if (listener != null) listener.onChange();
+        this.loggedIn = auth;
+        if (listener != null) listener.onChange();
     }
 }
--- web/www/public/JS/signup.js
+++ web/www/public/JS/signup.js
@@ -1,14 +1,9 @@
 window.addEventListener("load", () => {
-
-
     //to login
 
     var logInButton = document.querySelector('#logInButton');
 
     logInButton.addEventListener("click", function(evt) {
-
-
-
 
 
     })
