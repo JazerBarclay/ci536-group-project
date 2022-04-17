c85465a0ba2b70ee1703d536d58bd96dcb20a9a8
Jazer <contact@jazer.co.uk>
2022-04-17 23:40:45 +0100

    Added basic test for new post request

--- app/timer-java/src/test/java/timer/api/TestAPI.java
+++ app/timer-java/src/test/java/timer/api/TestAPI.java
@@ -2,22 +2,50 @@ package timer.api;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.HashMap;
+
 import org.junit.Test;
 
 public class TestAPI {
 
-  @Test
-  /**
-   * Test getRequest
-   */
-  public void testRequest() {
-    // Send request for images matching the term orion
-    HttpResponse res = API.request("https://images-api.nasa.gov/search?q=orion&media_type=image");
-    
-    // Assert 200 is received from response
-    assertEquals(200, res.getResponseCode());
-    
-    System.out.println(res.getBody());
-  }
+    @Test
+    /**
+     * Test getRequest
+     */
+    public void testGetRequest() {
+	// Send request for images matching the term orion
+	HttpResponse res = API.getRequest("https://images-api.nasa.gov/search?q=orion&media_type=image");
+
+	// Assert 200 is received from response
+	assertEquals(200, res.getResponseCode());
+
+	System.out.println(res.getBody());
+    }
+
+    @Test
+    /**
+     * Test getRequest
+     */
+    public void testPostRequest() {
+	// Send request for images matching the term orion
+	HttpResponse res = null;
+	try {
+	    res = API.postRequest("https://dev.api.quark.rocks/login", new HashMap<String, String>(){
+		private static final long serialVersionUID = 1L; {
+        	    put("email", "user@quark.rocks");
+        	    put("password", "pass");
+        	}});
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	
+	assertNotEquals(null, res);
+
+	// Assert 200 is received from response
+	assertEquals(200, res.getResponseCode());
+
+	System.out.println(res.getBody());
+    }
 
 }
