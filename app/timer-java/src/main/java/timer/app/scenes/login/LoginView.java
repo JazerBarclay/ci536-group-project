26386eb1910c620e2162e3de1e03fd706dbeb0f8
Jazer <contact@jazer.co.uk>
2022-04-17 23:42:08 +0100

    Added login functionality with token aquisition

--- app/timer-java/src/main/java/timer/app/scenes/login/LoginView.java
+++ app/timer-java/src/main/java/timer/app/scenes/login/LoginView.java
@@ -1,6 +1,7 @@
 package timer.app.scenes.login;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -20,6 +21,13 @@ import timer.fx.mvc.ScreenView;
  */
 public class LoginView extends ScreenView {
 
+    private GridPane gridLayout;
+    private Label message;
+    private TextField emailInput;
+    private PasswordField passwordInput;
+    private Button signupButton;
+    private Button loginButton;
+
     /**
      * Creates a new login interface with the given window, model and controller
      * 
@@ -30,6 +38,7 @@ public class LoginView extends ScreenView {
     public LoginView(Stage window, ScreenModel model, ScreenController controller) {
 	super(window, model, controller);
 	window.setTitle("Login");
+	window.setResizable(false);
     }
 
     /**
@@ -37,57 +46,94 @@ public class LoginView extends ScreenView {
      */
     @Override
     protected Scene constructView() {
-	
+
 	// Create grid layout container
-	GridPane gridLayout = new GridPane();
+	gridLayout = new GridPane();
 	gridLayout.setHgap(10);
 	gridLayout.setVgap(10);
-	gridLayout.setPadding(new Insets(30, 40, 40, 30));
-	
+	gridLayout.setPadding(new Insets(20, 40, 30, 30));
+
 	// Create username label for next text box
-	Label userLabel = new Label("Username");
-	
+	message = new Label("Login Failed - Please Try Again");
+	message.setMaxWidth(Double.MAX_VALUE);
+	message.setAlignment(Pos.CENTER);
+
+	// Create username label for next text box
+	Label emailLabel = new Label("Email");
+
 	// Create username input field
-	TextField userInput = new TextField();
-	userInput.setPromptText("Enter Username Here");
-	
+	emailInput = new TextField();
+	emailInput.setPromptText("Enter Email Here");
+
 	// Create password label for next password box
 	Label passwordLabel = new Label("Password");
-	
+
 	// Create password input field
-	PasswordField  passwordInput = new PasswordField();
+	passwordInput = new PasswordField();
 	passwordInput.setPromptText("Enter Password Here");
-	
+
 	// Create sign up button which takes you to the signup screen
-	Button signupButton = new Button("Signup");
+	signupButton = new Button("Signup");
 	signupButton.setPrefWidth(60);
-	signupButton.setOnAction(e -> {
-	    ((LoginController)controller).launchSignup(window);
-	});
 
 	// Create login button that attempts a login and forwards you to the app
-	Button loginButton = new Button("Login");
+	loginButton = new Button("Login");
 	loginButton.setPrefWidth(80);
-	loginButton.setOnAction(e -> {
-	    ((LoginController)controller).login(window);
-	});
 
 	// Add all elements to the grid layout (element, x, y, col-span, row-span)
-	gridLayout.add(userLabel, 0, 0);
-	gridLayout.add(userInput, 1, 0, 2, 1);
-	gridLayout.add(passwordLabel, 0, 1);
-	gridLayout.add(passwordInput, 1, 1, 2, 1);
-	gridLayout.add(signupButton, 1, 2, 1, 1);
-	gridLayout.add(loginButton, 2, 2, 1, 1);
-	
+	gridLayout.add(emailLabel, 0, 1);
+	gridLayout.add(emailInput, 1, 1, 2, 1);
+	gridLayout.add(passwordLabel, 0, 2);
+	gridLayout.add(passwordInput, 1, 2, 2, 1);
+	gridLayout.add(signupButton, 1, 3, 1, 1);
+	gridLayout.add(loginButton, 2, 3, 1, 1);
+
+	// Add action handlers set in defined function
+	setupActionHandlers();
+
 	// Return the grid layout containing all elements
 	return new Scene(gridLayout);
+
+    }
+
+    protected void setupActionHandlers() {
+
+	// TODO: Move functionality to login controller and store stats to model
+
+	// Handle login process
+	loginButton.setOnAction(e -> {
+
+	    // Remove any existing error label
+	    if (gridLayout.getChildren().contains(message)) gridLayout.getChildren().remove(message);
+
+	    // Resize window to fit content
+	    window.sizeToScene();
+
+	    // Try login
+	    boolean success = ((LoginController) controller).login(window, emailInput.getText(), passwordInput.getText());
+
+	    // If successful then exit here
+	    if (success) return;
+
+	    // Since we didnt succeed, add (potentially back) error message
+	    if (!gridLayout.getChildren().contains(message)) gridLayout.add(message, 0, 0, 3, 1);
+
+	    // Resize window to fit content
+	    window.sizeToScene();
+
+	});
+
+	// Handle signup process
+	signupButton.setOnAction(e -> {
+	    ((LoginController) controller).launchSignup(window);
+	});
+
     }
 
 
     @Override
     protected void update() {
-	// TODO: Add update based on login attempt details or something like that
+	// Currently usused since all buttons handle themselves for now
     }
 
 }
