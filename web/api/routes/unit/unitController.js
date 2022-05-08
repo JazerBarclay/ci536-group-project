014bb5eeddcdee24cc73979e257a07e2ac8f6535
Jazer <contact@jazer.co.uk>
2022-05-08 18:33:17 +0100

    Added label start and end to add unit request

--- web/api/routes/unit/unitController.js
+++ web/api/routes/unit/unitController.js
@@ -9,10 +9,7 @@ const { insertUnit } = require('./unitService')
 module.exports = {
 
     addUnit: (req, res) => {
-
-        // res.status(200).json({ id: req.body.auth.id, exp: req.body.auth.exp })
-
-        insertUnit(req.body.auth.id, 'test', '2022-03-25 13:33:15.903333', '2022-03-25 13:33:15.903333', (err, response) => {
+        insertUnit(req.body.auth.id, req.body.label, req.body.start, req.body.end, (err, response) => {
             // If database error, return 500 internal error
             if (err) return res.status(500).json({ err })
             return res.status(201).json()
