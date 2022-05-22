4161aa07d464dc534414bc5704dd58bf63d087ab
finlaywatling <finlaywatling@gmail.com>
2022-05-22 15:36:57 +0100

    selectAllUnits and getUnits implemented in api/routes/unit

--- web/api/routes/unit/unitRouter.js
+++ web/api/routes/unit/unitRouter.js
@@ -5,7 +5,7 @@
 
 // Import modules from controller here
 const { validateToken } = require('../../authentication/tokenValidation')
-const { addUnit, getUnit } = require('./unitController')
+const { addUnit, getUnit, getUnits } = require('./unitController')
 
 
 // Add router from express to manage routing
@@ -16,6 +16,9 @@ router.get('/', validateToken, getUnit)
 
 // Insert to /unit
 router.post('/', validateToken, addUnit)
+
+// Read all from /unit
+router.post('/', validateToken, getUnits)
 
 // Export this router module
 module.exports = router;
