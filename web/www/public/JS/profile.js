12d1ba8e4342f8b3a8371132a95ee606376b0b2f
Jazer <contact@jazer.co.uk>
2022-05-23 13:57:01 +0100

    Updated profile layout with core and nav css

--- web/www/public/JS/profile.js
+++ web/www/public/JS/profile.js
@@ -1,95 +1,19 @@
 window.addEventListener('load', () => {
 
     if (localStorage.token == undefined) { //making sure that the user has generated a login token before visiting profile
-
         console.log("invalid token!")
-
-        window.location.href = "index.html"
-
-
-    } else {
-
-        console.log("This is a valid session using token: " + localStorage.token); //temporary output used for development
-
-
+        window.location.href = "login.html"
     }
+
+    console.log("This is a valid session using token: " + localStorage.token); //temporary output used for development
 
     var graphLink = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"
 
-
-    const xValues = ["mon", 'tues', 'weds', 'thurs', 'fri', 'sat', 'sun'];
+    const xValues = ["Mon", 'Tues', 'Weds', 'Thurs', 'Fri', 'Sat', 'Sun'];
 
     //The following data will be eventually passed to the graph from the database
     var thisWeekData = [1, 0, 2, 0, 0, 3, 4];
     var lastWeekData = [4, 6, 3, 2, 1, 4, 3];
-
-    //TODO: 
-
-    function getUserDetails(){
-
-        var url = "https://dev.api.quark.rocks/user";
-    
-            var myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-    
-            var raw = JSON.stringify({
-                "username": username,
-            });
-    
-            var requestOptions = {
-                method: 'GET',
-                headers: myHeaders,
-                body: raw,
-                redirect: 'follow'
-            };
-    
-            fetch(url, requestOptions)
-                .then(response => response.text())
-                .then(result => {
-
-                    console.log(result);
-
-                })
-                .catch(error => console.log('error', error));
-
-    }
-
-    function getUserUnits() {
-        
-
-            var url = "https://dev.api.quark.rocks/unit";
-    
-            var myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-    
-            var raw = JSON.stringify({
-                "username": username,
-            });
-    
-            var requestOptions = {
-                method: 'GET',
-                headers: myHeaders,
-                body: raw,
-                redirect: 'follow'
-            };
-    
-            fetch(url, requestOptions)
-                .then(response => response.text())
-                .then(result => {
-
-                    console.log(result);
-
-                })
-                .catch(error => console.log('error', error));
-    
-   
-
-    }
-
-
-
-
-
 
     new Chart("graph", { //generating the chart using user input (thisweekdata and lastweekdata)
         type: "line",
@@ -100,9 +24,7 @@ window.addEventListener('load', () => {
                     data: thisWeekData,
                     borderColor: "black",
                     fill: true
-                },
-                {
-
+                }, {
                     label: 'Completed Blocks Last Week',
                     data: lastWeekData,
                     borderColor: "light grey",
@@ -116,5 +38,64 @@ window.addEventListener('load', () => {
         }
     });
 
-
 })
+
+
+function getUserDetails(){
+
+    var url = "https://dev.api.quark.rocks/user";
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "username": username,
+        });
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch(url, requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => console.log('error', error));
+
+}
+
+function getUserUnits() {
+    
+
+        var url = "https://dev.api.quark.rocks/unit";
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "username": username,
+        });
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch(url, requestOptions)
+            .then(response => response.text())
+            .then(result => {
+
+                console.log(result);
+
+            })
+            .catch(error => console.log('error', error));
+
+
+
+}
