1ad965dac05a60d7b9da0dc31a37ab36e8b30446
Jazer <contact@jazer.co.uk>
2022-05-26 14:47:13 +0100

    Added signup functionality

--- app/timer-java/src/main/java/timer/app/scenes/signup/SignupController.java
+++ app/timer-java/src/main/java/timer/app/scenes/signup/SignupController.java
@@ -1,7 +1,15 @@
 package timer.app.scenes.signup;
 
+import java.io.IOException;
+import java.util.HashMap;
+
 import javafx.stage.Stage;
+import timer.api.API;
+import timer.api.HttpResponse;
+import timer.app.Launcher;
+import timer.app.Mode;
 import timer.app.scenes.login.LoginScreen;
+import timer.app.scenes.timer.TimerScreen;
 import timer.fx.mvc.ScreenController;
 import timer.fx.mvc.ScreenModel;
 
@@ -27,6 +35,61 @@ public class SignupController extends ScreenController {
    */
   public void launchLogin(Stage window) {
     new LoginScreen(window);
+  }
+
+  public boolean signup(Stage window, String email, String username, String password) {
+    
+    // Ensure email is valid
+    if (!validateEmail(email))
+      return false;
+    
+ // Send POST request to API
+    HttpResponse res = null;
+
+    // API endpoint based on launch conditions
+    String endPoint = "";
+    if (Launcher.state == Mode.DEV)
+      endPoint = "https://dev.api.quark.rocks/register";
+    else if (Launcher.state == Mode.PRODUCTION)
+      endPoint = "https://api.quark.rocks/register";
+    else
+      endPoint = "http://[::1]:4000/register";
+
+    try {
+      res = API.postRequest(endPoint, new HashMap<String, String>() {
+        private static final long serialVersionUID = 1L;
+        {
+          put("email", email);
+          put("username", username);
+          put("password", password);
+        }
+      });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    
+    // If we error out and res remains null, return
+    if (res == null)
+      return false;
+
+    // If we got a response other than 200 then return
+    if (res.getResponseCode() != 201)
+      return false;
+
+    // Open the timer screen with the login token
+    new LoginScreen(new Stage());
+
+    // Close this window now we are done!
+    window.close();
+
+    return true;
+    
+  }
+
+  //TODO: Add validation
+  private boolean validateEmail(String email) {
+    return true;
   }
 
 }
--- app/timer-java/src/main/java/timer/app/scenes/signup/SignupView.java
+++ app/timer-java/src/main/java/timer/app/scenes/signup/SignupView.java
@@ -1,6 +1,7 @@
 package timer.app.scenes.signup;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,6 +12,7 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import timer.app.Launcher;
 import timer.app.Mode;
+import timer.app.scenes.login.LoginController;
 import timer.fx.mvc.ScreenController;
 import timer.fx.mvc.ScreenModel;
 import timer.fx.mvc.ScreenView;
@@ -23,8 +25,14 @@ import timer.fx.mvc.ScreenView;
  */
 public class SignupView extends ScreenView {
 
+  private GridPane gridLayout;
+  private Label message;
+  private TextField emailInput;
+  private TextField userInput;
+  private PasswordField passwordInput;
+  private Button loginButton;
   private Button createButton;
-  
+
   /**
    * Creates a new signup interface with the given window, model and controller
    * 
@@ -43,70 +51,108 @@ public class SignupView extends ScreenView {
   @Override
   protected Scene constructView() {
     // Create grid layout container
-    GridPane gridLayout = new GridPane();
+    gridLayout = new GridPane();
     gridLayout.setHgap(10);
     gridLayout.setVgap(10);
     gridLayout.setPadding(new Insets(30, 40, 40, 30));
 
+    // Create username label for next text box
+    message = new Label("Signup Failed - Email or Username in use");
+    message.setMaxWidth(Double.MAX_VALUE);
+    message.setAlignment(Pos.CENTER);
+    
     // Create email label for next text box
     Label emailLabel = new Label("Email");
 
     // Create username input field
-    TextField emailInput = new TextField();
+    emailInput = new TextField();
     emailInput.setPromptText("Enter Email Here");
 
     // Create username label for next text box
     Label userLabel = new Label("Username");
 
     // Create username input field
-    TextField userInput = new TextField();
+    userInput = new TextField();
     userInput.setPromptText("Enter Username Here");
 
     // Create password label for next password box
     Label passwordLabel = new Label("Password");
 
     // Create password input field
-    PasswordField passwordInput = new PasswordField();
+    passwordInput = new PasswordField();
     passwordInput.setPromptText("Enter Password Here");
+
+
+    // Create sign up button which takes you to the signup screen
+    loginButton = new Button("Login");
+    loginButton.setPrefWidth(60);
+
+
+    // Create login button that attempts a login and forwards you to the app
+    createButton = new Button("Create");
+    createButton.setPrefWidth(80);
+
+
+    // Add all elements to the grid layout (element, x, y, col-span, row-span)
+    gridLayout.add(emailLabel, 0, 1);
+    gridLayout.add(emailInput, 1, 1, 2, 1);
+    gridLayout.add(userLabel, 0, 2);
+    gridLayout.add(userInput, 1, 2, 2, 1);
+    gridLayout.add(passwordLabel, 0, 3);
+    gridLayout.add(passwordInput, 1, 3, 2, 1);
+    gridLayout.add(loginButton, 1, 4, 1, 1);
+    gridLayout.add(createButton, 2, 4, 1, 1);
+
+    // Add action handlers set in defined function
+    setupActionHandlers();
+    
+    // Return the grid layout containing all elements
+    return new Scene(gridLayout);
+  }
+  
+  protected void setupActionHandlers() {
+
+    // TODO: Move functionality to controller and store stats to model
+
+    // Handle signup process
+    createButton.setOnAction(e -> {
+      if (gridLayout.getChildren().contains(message))
+        gridLayout.getChildren().remove(message);
+      
+      // Resize window to fit content
+      window.sizeToScene();
+      
+      boolean success = ((SignupController) controller).signup(window, emailInput.getText(), userInput.getText(), passwordInput.getText());
+      
+      // If successful then exit here
+      if (success)
+        return;
+      
+      // Since we didnt succeed, add (potentially back) error message
+      if (!gridLayout.getChildren().contains(message))
+        gridLayout.add(message, 0, 0, 3, 1);
+
+      // Resize window to fit content
+      window.sizeToScene();
+      
+    });
+    
     passwordInput.setOnKeyPressed(e -> {
       if (e.getCode() == KeyCode.ENTER) {
         createButton.fire();
       }
     });
-
-    // Create sign up button which takes you to the signup screen
-    Button loginButton = new Button("Login");
-    loginButton.setPrefWidth(60);
+    
+    // Go to login screen
     loginButton.setOnAction(e -> {
       ((SignupController) controller).launchLogin(window);
     });
 
-    // Create login button that attempts a login and forwards you to the app
-    createButton = new Button("Create");
-    createButton.setPrefWidth(80);
-    createButton.setOnAction(e -> {
-      
-      
-      
-    });
-
-    // Add all elements to the grid layout (element, x, y, col-span, row-span)
-    gridLayout.add(emailLabel, 0, 0);
-    gridLayout.add(emailInput, 1, 0, 2, 1);
-    gridLayout.add(userLabel, 0, 1);
-    gridLayout.add(userInput, 1, 1, 2, 1);
-    gridLayout.add(passwordLabel, 0, 2);
-    gridLayout.add(passwordInput, 1, 2, 2, 1);
-    gridLayout.add(loginButton, 1, 3, 1, 1);
-    gridLayout.add(createButton, 2, 3, 1, 1);
-
-    // Return the grid layout containing all elements
-    return new Scene(gridLayout);
   }
 
   @Override
   protected void update() {
-
+    // Currently usused since all buttons handle themselves for now
   }
 
 }
