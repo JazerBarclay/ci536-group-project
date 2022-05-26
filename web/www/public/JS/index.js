83b1a9bcc456e9db44208ac7fa5dbfd72c0da48c
Jazer <contact@jazer.co.uk>
2022-05-26 22:37:25 +0100

    Added check for login status that changes nav

--- web/www/public/JS/index.js
+++ web/www/public/JS/index.js
@@ -6,4 +6,12 @@ window.addEventListener('load', () =>{
         window.location.href = "signup.html"
     })
 
+    var login = document.querySelector('#navLogin')
+
+    if (window.localStorage.token){
+        login.textContent = "Profile"
+        login.href = "profile.html"
+    }
+
+
 })
