16b6435aae86086a177a95eb725071c3d81c5301
Jazer <contact@jazer.co.uk>
2022-05-26 23:48:16 +0100

    Added link to download from page

--- web/www/public/JS/index.js
+++ web/www/public/JS/index.js
@@ -1,9 +1,14 @@
 window.addEventListener('load', () =>{
 
     var signUpButton = document.querySelector('#sign-up');
+    var downloadButton = document.querySelector('#downloadButton');
 
     signUpButton.addEventListener('click', evt =>{
         window.location.href = "signup.html"
+    })
+
+    downloadButton.addEventListener('click', evt =>{
+        window.location.href = "download.html"
     })
 
     var login = document.querySelector('#navLogin')
