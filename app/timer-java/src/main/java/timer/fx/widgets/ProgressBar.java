0adf6387c9396443ec106521ff7b8188a67a1e68
Jazer <contact@jazer.co.uk>
2022-05-26 21:16:49 +0100

    Fixed progress bar generation

--- app/timer-java/src/main/java/timer/fx/widgets/ProgressBar.java
+++ app/timer-java/src/main/java/timer/fx/widgets/ProgressBar.java
@@ -5,7 +5,7 @@ import javafx.scene.shape.Ellipse;
 
 public class ProgressBar extends CanvasClickButton {
 
-    int count;
+    int count = 0;
 
     public ProgressBar(int x, int y, int width, int height) {
         super(x, y, width, height);
@@ -24,10 +24,27 @@ public class ProgressBar extends CanvasClickButton {
     public ProgressBar() {
         this(0, 0);
     }
+    
+    public void increment() {
+      count++;
+    }
+    
+    public void decrement() {
+      count--;
+    }
+    
+    public void setCount(int count) {
+      this.count = count;
+    }
 
     @Override
     public void drawClicked() {
        gc.setStroke(Color.BLACK);
-       gc.strokeRect(0,0,getWidth(),getHeight());
+       int w = 8, p = 4;
+       for (int i = 0; i < count; i++) {
+         gc.strokeOval(getWidth()/2 - (count*(w+p)) / 2+i*(w+p) + p/2, getHeight()/2-w/2, w, w);
+//         gc.strokeOval(getWidth()/2 - (count*(w+p)) / 2+i*(w+p) + p/2 , getHeight()/2-w/2, w, w);               , 2, 8, 8);
+       }
     }
+    
 }
